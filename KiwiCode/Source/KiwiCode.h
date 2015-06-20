87e41a215599b9101839a753f931dcd7d4d9bc58
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-06-21 08:37:43 +0900

    build: Initial commit

---
+++ KiwiCode/Source/KiwiCode.h
@@ -0,0 +1,19 @@
+//
+//  KiwiCode.h
+//  KiwiCode
+//
+//  Created by Tomoo Hamada on 2015/06/20.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KiwiCode.
+FOUNDATION_EXPORT double KiwiCodeVersionNumber;
+
+//! Project version string for KiwiCode.
+FOUNDATION_EXPORT const unsigned char KiwiCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KiwiCode/PublicHeader.h>
+
+
