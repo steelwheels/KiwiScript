e0099307a5860737a64e0cd361b0501c503054df
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-06-21 14:13:44 +0900

    application: Update "jsexec" command

---
+++ KiwiEngine/Application/jsexec/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  jsx
+//
+//  Created by Tomoo Hamada on 2015/06/20.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
+
+func main () -> Int
+{
+	var result : Int = 0 ;
+	let arguments = NSProcessInfo.processInfo().arguments
+	if(arguments.count == 2){
+		
+	} else {
+		
+	}
+	
+	for arg in arguments {
+		println("arg \(arg)")
+	}
+	return result ;
+}
+
+main()
+
