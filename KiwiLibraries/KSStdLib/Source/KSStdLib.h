6e7dee70c0096fc7cc6e5bd4ccb6dd0ba3fe52c0
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-08-23 22:08:48 +0900

    KLStdLib: Rename to KSStdLib

---
+++ KiwiLibraries/KSStdLib/Source/KSStdLib.h
@@ -0,0 +1,19 @@
+//
+//  KSStdLib.h
+//  KSStdLib
+//
+//  Created by Tomoo Hamada on 2015/08/22.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KSStdLib.
+FOUNDATION_EXPORT double KSStdLibVersionNumber;
+
+//! Project version string for KSStdLib.
+FOUNDATION_EXPORT const unsigned char KSStdLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSStdLib/PublicHeader.h>
+
+
