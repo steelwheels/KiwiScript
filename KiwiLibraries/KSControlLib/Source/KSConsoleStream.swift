62145d4483f8b8eff513d9fdb04e9dc522f73b93
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-08-25 02:23:27 +0900

    KSControlLib: Initial commit

---
+++ KiwiLibraries/KSControlLib/Source/KSConsoleStream.swift
@@ -0,0 +1,9 @@
+//
+//  KSConsoleStream.swift
+//  KSControlLib
+//
+//  Created by Tomoo Hamada on 2015/08/25.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
---
+++ KiwiLibraries/KSControlLib/Source/KSControlLib.h
@@ -0,0 +1,18 @@
+/**
+ * @file	KSControlLib.h
+ * @brief	Header file for KSControlLib framework
+ * @par Copyright
+ *   Copyright (C) 2015 Steel Wheels Project
+ */
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KSControlLib.
+FOUNDATION_EXPORT double KSControlLibVersionNumber;
+
+//! Project version string for KSControlLib.
+FOUNDATION_EXPORT const unsigned char KSControlLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSControlLib/PublicHeader.h>
+
+
