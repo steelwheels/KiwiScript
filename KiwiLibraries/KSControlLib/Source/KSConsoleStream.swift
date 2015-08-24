0d5590f1af62415794a0ab009f5720e5e00deb6f
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-08-25 02:24:15 +0900

    KSControlLib: Update KSConsoleStream class

--- KiwiLibraries/KSControlLib/Source/KSConsoleStream.swift
+++ KiwiLibraries/KSControlLib/Source/KSConsoleStream.swift
@@ -1,9 +1,25 @@
-//
-//  KSConsoleStream.swift
-//  KSControlLib
-//
-//  Created by Tomoo Hamada on 2015/08/25.
-//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
-//
+/**
+* @file		KSConsoleStream.swift
+* @brief	Define KSConsoleStream class
+* @par Copyright
+*   Copyright (C) 2015 Steel Wheels Project
+*/
 
-import Foundation
+import KSStdLib
+import KCConsoleView
+
+public class KSConsoleStream : KSOutputStream
+{
+	var consoleView : KCConsoleView
+	
+	public init(view : KCConsoleView){
+		self.consoleView = view
+		super.init()
+	}
+	
+	public override func putString(str : String){
+		consoleView.appendText(str)
+	}
+}
+
+
