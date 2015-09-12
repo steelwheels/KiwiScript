32bbbe03607908639f20568e4074b6a66d20da66
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-09-12 17:03:27 +0900

    KiwiEngine: Remove KEError class
    The KEError class is replaced by CNError class in Canary framework.

---
+++ KSStdLib/Source/KSValueExtension.swift
@@ -0,0 +1,9 @@
+//
+//  KSValueExtension.swift
+//  KSStdLib
+//
+//  Created by Tomoo Hamada on 2015/09/12.
+//  Copyright © 2015年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
