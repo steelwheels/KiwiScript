2a12730a7075863b6e06944c37ea104c91821713
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-09-14 00:07:00 +0900

    JSRunner: Update project setting

---
+++ JSRunner/Source/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  JSRunner
+//
+//  Created by Tomoo Hamada on 2015/09/13.
+//  Copyright (c) 2015年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
+import Canary
+import KiwiEngine
+import KSStdLib
+
+print("Hello, World!", terminator: "")
+
