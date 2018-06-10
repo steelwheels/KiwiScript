3d85229a09beee279a55a82d8d66686f7baf2215
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-06-10 09:41:39 +0900

    KiwiLibrary: Add phase to check result of unit test

---
+++ Script/unit_test.mk
@@ -0,0 +1,11 @@
+#
+# unit_test.mk
+#
+
+build_dir = $(BUILD_DIR)/$(CONFIGURATION)$(EFFECTIVE_PLATFORM_NAME)
+test_exec = $(build_dir)/UnitTest
+
+all:
+	$(test_exec) 2>&1 | tee $(build_dir)/UnitTest.log
+	diff $(build_dir)/UnitTest.log ../Test/UnitTest.log.OK
+
