1676d8743ee32dd3fa894a08211079b3e4edfc5b
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-09-22 01:26:46 +0900

    KiwiScript: Update for Xcode10

--- Script/install_osx.mk
+++ Script/install_osx.mk
@@ -5,9 +5,6 @@
 INSTALL_PATH ?= $(HOME)/Library/Frameworks
 
 install: dummy
-	xcodebuild build -target $(PROJECT_NAME) \
-	  -project $(PROJECT_NAME).xcodeproj \
-	  -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO
 	xcodebuild install -target $(PROJECT_NAME) \
 	  -project $(PROJECT_NAME).xcodeproj \
 	  -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO
