ddd20fd1844b7d409dbc6e98dd8dbe148a965dc7
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-04-14 23:16:13 +0900

    KiwiObject: Update KMDefaultFunction class to hold log-console

--- KiwiObject/Source/KMFunction.swift
+++ KiwiObject/Source/KMFunction.swift
@@ -6,6 +6,7 @@
  */
 
 import KiwiEngine
+import CoconutData
 import JavaScriptCore
 import Foundation
 
@@ -21,19 +22,29 @@ open class KMDefaultFunction: KMFunction
 	private var mSourceCode:	String
 	private var mFunctionObject:	JSValue
 	private var mContext:		KEContext
+	private var mConsole:		CNConsole
 
 	public var sourceCode: String 		{ get { return mSourceCode }}
 	public var functionObject: JSValue	{ get { return mFunctionObject }}
 	public var context: KEContext		{ get { return mContext }}
 
-	public init(sourceCode code: String, functionObject obj: JSValue, context ctxt: KEContext){
+	public init(sourceCode code: String, functionObject obj: JSValue, context ctxt: KEContext, console cons: CNConsole){
 		mSourceCode	= code
 		mFunctionObject	= obj
 		mContext	= ctxt
+		mConsole	= cons
 	}
 
 	public func call(parameters params: Array<JSValue>) -> JSValue? {
 		return mFunctionObject.call(withArguments: params)
+	}
+
+	public func log(message msg: String){
+		mConsole.print(string: msg + "\n")
+	}
+
+	public func error(message msg: String){
+		mConsole.print(string: msg + "\n")
 	}
 }
 
