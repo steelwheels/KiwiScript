6c928aeeddd72172bed9c189d955d6a25d93fe57
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-07-17 23:59:59 +0900

    KiwiLibrary: Update Graphics module
    Graphics.insetRect:	Get inside rectangle of source rectangle

--- KiwiLibrary/Resource/Library/Graphics.js
+++ KiwiLibrary/Resource/Library/Graphics.js
@@ -1,10 +1,16 @@
 /* Graphics.js */
 
-// func InsetRect(rect: Rect, dx:Double, dy:Double) -> Rect
-function insetRect(rect, dx, dy) {
-	const newx      = rect.x + dx ;
-	const newy      = rect.y + dy ;
-	const newwidth  = max(rect.width  - 2.0 * dx, 0.0) ;
-	const newheight = max(rect.height - 2.0 * dy, 0.0) ;
-	return Rect(newx, newy, newwidth, newheight) ;
+class Graphics {
+	// func InsetRect(rect: Rect, dx:Double, dy:Double) -> Rect
+	static insetRect(rect, dx, dy) {
+		if(checkVariables("insetRect", rect, dx, dy)){
+			const newx      = rect.x + dx ;
+			const newy      = rect.y + dy ;
+			const newwidth  = Math.max(rect.width  - 2.0 * dx, 0.0) ;
+			const newheight = Math.max(rect.height - 2.0 * dy, 0.0) ;
+			return Rect(newx, newy, newwidth, newheight) ;
+		} else {
+			return Rect(0.0, 0.0, 0.0, 0.0) ;
+		}
+	}
 }
