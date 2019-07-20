e741a6b5a9ed673a9882fe7835b915e69a611e81
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-07-20 11:06:35 +0900

    KiwiLibrary: Update Grapics.js
    Following functions are added:
    	centerOfRect
    	pointInFrame

--- KiwiLibrary/Resource/Library/Graphics.js
+++ KiwiLibrary/Resource/Library/Graphics.js
@@ -13,4 +13,28 @@ class Graphics {
 			return Rect(0.0, 0.0, 0.0, 0.0) ;
 		}
 	}
+
+	// func centerOfRect(rect: Rect) -> Point
+	static centerOfRect(rect){
+		const x = rect.x + (rect.width  / 2.0) ;
+		const y = rect.y + (rect.height / 2.0) ;
+		return Point(x, y) ;
+	}
+
+	// func pointInFrame(point: Point, frame: Rect) -> Point
+	static pointInFrame(point, frame){
+		let x = point.x ;
+		if(x < frame.x){
+			x = frame.x ;
+		} else if(frame.x + frame.width < x){
+			x = frame.x + frame.width ;
+		}
+		let y = point.y ;
+		if(y < frame.y){
+			y = frame.y ;
+		} else if(frame.y + frame.height < y){
+			y = frame.y + frame.height ;
+		}
+		return Point(x, y) ;
+	}
 }
