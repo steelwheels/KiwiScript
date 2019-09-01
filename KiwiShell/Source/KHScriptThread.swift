22a15b5320963789570f24f885718b10b29d0de5
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-09-01 17:37:43 +0900

    KiwiShell: Support "waitUntilExit" method by KHScriptThread class

--- KiwiShell/Source/KHScriptThread.swift
+++ KiwiShell/Source/KHScriptThread.swift
@@ -20,18 +20,26 @@ import Foundation
 
 @objc public class KHScriptThread: CNPipeThread, KHThreadProtocol
 {
+	public enum ThreadState {
+		case Initial
+		case Running
+		case Finished
+	}
+
 	public static let EnvironmentItem	= "_env"
 
 	private var mContext:			KEContext
 	private var mScripts:			Array<URL>
 	private var mArguments:			Array<String>
 	private var mTerminationStatus:		Int32
+	private var mThreadState:		ThreadState
 
 	public init(virtualMachine vm: JSVirtualMachine, shellInterface intf: CNShellInterface, environment env: CNShellEnvironment, console cons: CNConsole, config conf: KHConfig){
 		mContext		= KEContext(virtualMachine: vm)
 		mScripts		= []
 		mArguments		= []
 		mTerminationStatus	= 1
+		mThreadState		= .Initial
 		super.init(interface: intf, environment: env, console: cons, config: conf)
 
 		/* Compile the context */
@@ -51,19 +59,25 @@ import Foundation
 		compiler.defineBuiltinFunctions(parentThread: self, context: mContext)
 	}
 
+	public var threadState: ThreadState {
+		get { return mThreadState }
+	}
+
 	public override var terminationStatus: Int32 {
 		get { return mTerminationStatus }
 	}
 
 	public func start(userScripts scripts: Array<URL>, arguments args: Array<String>) {
-		mScripts   = scripts
-		mArguments = args
+		mScripts   	= scripts
+		mArguments 	= args
+		mThreadState	= .Running
 		super.start()
 	}
 
 	public override func main() {
 		guard let conf = config as? KHConfig else {
 			NSLog("Can not happen")
+			mThreadState = .Finished
 			return
 		}
 
@@ -72,6 +86,7 @@ import Foundation
 		guard compiler.compile(context: mContext, sourceFiles: mScripts, console: console, config: conf) else {
 			console.error(string: "Failed to compile  user scripts")
 			mTerminationStatus = 1
+			mThreadState       = .Finished
 			return
 		}
 
@@ -92,6 +107,13 @@ import Foundation
 			}
 		} else {
 			mTerminationStatus = 0
+		}
+		mThreadState = .Finished
+	}
+
+	public func waitUntilExit() {
+		while self.threadState != .Finished {
+			Thread.sleep(forTimeInterval: 1/10000.0) // 0.1ms
 		}
 	}
 }
