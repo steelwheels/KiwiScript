3d5d0928aad9d6fa8ef40dfff330d8dd7c4ae5b1
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2021-11-07 10:01:31 +0900

    KiwiLibrary: Support new symbols: rectangle,

--- KiwiLibrary/Source/File/KLSymbol.swift
+++ KiwiLibrary/Source/File/KLSymbol.swift
@@ -16,9 +16,16 @@ import Foundation
 	var chevronBackward:	KLURL { get }
 	var chevronForward:	KLURL { get }
 	var handRaised:		KLURL { get }
+	var line1P:		KLURL { get }
+	var line2P:		KLURL { get }
+	var line4P:		KLURL { get }
+	var line8P:		KLURL { get }
+	var line16P:		KLURL { get }
 	var paintbrush:		KLURL { get }
 	var pencil:		KLURL { get }
 	var questionmark:	KLURL { get }
+	var rectangle:		KLURL { get }
+	var rectangleFilled:	KLURL { get }
 }
 
 @objc public class KLSymbol: NSObject, KLSymbolProtocol
@@ -33,9 +40,16 @@ import Foundation
 	public var chevronBackward: KLURL { get { return URLOfSymbol(type: .chevronBackward)	}}
 	public var chevronForward:  KLURL { get { return URLOfSymbol(type: .chevronForward)  	}}
 	public var handRaised:      KLURL { get { return URLOfSymbol(type: .handRaised)		}}
+	public var line1P: 	    KLURL { get { return URLOfSymbol(type: .line1P)		}}
+	public var line2P: 	    KLURL { get { return URLOfSymbol(type: .line2P)		}}
+	public var line4P: 	    KLURL { get { return URLOfSymbol(type: .line4P)		}}
+	public var line8P: 	    KLURL { get { return URLOfSymbol(type: .line8P)		}}
+	public var line16P: 	    KLURL { get { return URLOfSymbol(type: .line16P)		}}
 	public var paintbrush:      KLURL { get { return URLOfSymbol(type: .paintbrush)		}}
 	public var pencil:	    KLURL { get { return URLOfSymbol(type: .pencil)		}}
 	public var questionmark:    KLURL { get { return URLOfSymbol(type: .questionmark)	}}
+	public var rectangle:	    KLURL { get { return URLOfSymbol(type: .rectangle)		}}
+	public var rectangleFilled: KLURL { get { return URLOfSymbol(type: .rectangleFilled)	}}
 
 	private func URLOfSymbol(type typ: CNSymbol.SymbolType) -> KLURL {
 		let url = CNSymbol.shared.URLOfSymbol(type: typ)
