a0b46300a02e77819937c74fba0edee270522fe9
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-06-04 08:44:08 +0900

    KiwiLibrary: Update builtin declaration: Enum.d.ts

--- KiwiShell/Resource/Library/types/KiwiLibrary.d.ts
+++ KiwiShell/Resource/Library/types/KiwiLibrary.d.ts
@@ -491,72 +491,6 @@ declare class Turtle {
 /// <reference path="Process.d.ts" />
 /// <reference path="Enum.d.ts" />
 declare function requestContactAccess(): boolean;
-declare enum LogLevel {
-  warning = 2,
-  nolog = 0,
-  error = 1,
-  detail = 4,
-  debug = 3
-}
-declare namespace LogLevel {
-  function description(param: LogLevel): string ;
-}
-declare enum AnimationState {
-  run = 1,
-  pause = 2,
-  idle = 0
-}
-declare namespace AnimationState {
-  function description(param: AnimationState): string ;
-}
-declare enum ValueType {
-  arrayType = 11,
-  boolType = 1,
-  URLType = 12,
-  numberType = 2,
-  segmentType = 17,
-  imageType = 14,
-  sizeType = 7,
-  nullType = 0,
-  recordType = 15,
-  dateType = 4,
-  objectType = 16,
-  rectType = 8,
-  enumType = 9,
-  colorType = 13,
-  pointType = 6,
-  dictionaryType = 10,
-  rangeType = 5,
-  stringType = 3
-}
-declare namespace ValueType {
-  function description(param: ValueType): string ;
-}
-declare enum ExitCode {
-  commaneLineError = 2,
-  internalError = 1,
-  exception = 4,
-  noError = 0,
-  syntaxError = 3
-}
-declare namespace ExitCode {
-  function description(param: ExitCode): string ;
-}
-declare enum AccessType {
-  append = 2,
-  read = 0,
-  write = 1
-}
-declare namespace AccessType {
-  function description(param: AccessType): string ;
-}
-declare enum Axis {
-  horizontal = 0,
-  vertical = 1
-}
-declare namespace Axis {
-  function description(param: Axis): string ;
-}
 declare enum FileType {
   directory = 2,
   notExist = 0,
@@ -564,6 +498,35 @@ declare enum FileType {
 }
 declare namespace FileType {
   function description(param: FileType): string ;
+}
+declare enum LogLevel {
+  error = 1,
+  debug = 3,
+  detail = 4,
+  nolog = 0,
+  warning = 2
+}
+declare namespace LogLevel {
+  function description(param: LogLevel): string ;
+}
+declare enum ExitCode {
+  exception = 4,
+  internalError = 1,
+  noError = 0,
+  commaneLineError = 2,
+  syntaxError = 3
+}
+declare namespace ExitCode {
+  function description(param: ExitCode): string ;
+}
+declare enum Alignment {
+  fill = 2,
+  trailing = 1,
+  leading = 0,
+  center = 3
+}
+declare namespace Alignment {
+  function description(param: Alignment): string ;
 }
 declare enum FontSize {
   regular = 13,
@@ -573,39 +536,76 @@ declare enum FontSize {
 declare namespace FontSize {
   function description(param: FontSize): string ;
 }
+declare enum ValueType {
+  recordType = 15,
+  stringType = 3,
+  dateType = 4,
+  arrayType = 11,
+  URLType = 12,
+  colorType = 13,
+  imageType = 14,
+  boolType = 1,
+  segmentType = 17,
+  objectType = 16,
+  sizeType = 7,
+  nullType = 0,
+  numberType = 2,
+  dictionaryType = 10,
+  rangeType = 5,
+  enumType = 9,
+  rectType = 8,
+  pointType = 6
+}
+declare namespace ValueType {
+  function description(param: ValueType): string ;
+}
 declare enum TextAlign {
-  justfied = 3,
-  normal = 4,
   right = 1,
+  center = 2,
   left = 0,
-  center = 2
+  justfied = 3,
+  normal = 4
 }
 declare namespace TextAlign {
   function description(param: TextAlign): string ;
 }
-declare enum Distribution {
-  equalSpacing = 3,
-  fillProportinally = 1,
-  fillEqually = 2,
-  fill = 0
-}
-declare namespace Distribution {
-  function description(param: Distribution): string ;
-}
 declare enum Authorize {
-  undetermined = 0,
   authorized = 3,
+  undetermined = 0,
   denied = 2
 }
 declare namespace Authorize {
   function description(param: Authorize): string ;
 }
-declare enum Alignment {
-  center = 3,
-  trailing = 1,
-  leading = 0,
-  fill = 2
+declare enum AnimationState {
+  pause = 2,
+  idle = 0,
+  run = 1
 }
-declare namespace Alignment {
-  function description(param: Alignment): string ;
+declare namespace AnimationState {
+  function description(param: AnimationState): string ;
+}
+declare enum Distribution {
+  fill = 0,
+  equalSpacing = 3,
+  fillProportinally = 1,
+  fillEqually = 2
+}
+declare namespace Distribution {
+  function description(param: Distribution): string ;
+}
+declare enum AccessType {
+  write = 1,
+  read = 0,
+  append = 2
+}
+declare namespace AccessType {
+  function description(param: AccessType): string ;
+}
+declare enum Axis {
+  vertical = 1,
+  horizontal = 0
+}
+declare namespace Axis {
+  function description(param: Axis): string ;
 }
