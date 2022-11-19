ad155faabc7e8a22672c3897766c75b43159a548
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-11-19 23:17:33 +0900

    KiwiTools: Initial commit

---
+++ KiwiTools/Source/edecl/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  KiwiTools
+//
+//  Created by Tomoo Hamada on 2022/11/19.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Script/install_tool.mk
@@ -0,0 +1,67 @@
+# install_xc.mk
+
+BUNDLE_PATH	= $(HOME)/tools/bundles
+BIN_PATH	= $(HOME)/tools/bin
+
+all: dummy
+	echo "usage: make install_bundle or make install_tools"
+
+install_bundle: dummy
+	xcodebuild install \
+	  -scheme $(PROJECT_NAME)Bundle \
+	  -project $(PROJECT_NAME).xcodeproj \
+	  -destination="macOSX" \
+	  -configuration Release \
+	  -sdk macosx \
+	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+	  INSTALL_PATH=$(BUNDLE_PATH) \
+	  SKIP_INSTALL=NO \
+	  DSTROOT=/ \
+	  ONLY_ACTIVE_ARCH=NO
+
+install_tools: install_asc install_adecl install_ares
+
+install_asc: dummy
+	xcodebuild install \
+	  -scheme $(PROJECT_NAME) \
+	  -project $(PROJECT_NAME).xcodeproj \
+	  -destination="macOSX" \
+	  -configuration Release \
+	  -sdk macosx \
+	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+	  INSTALL_PATH=$(BIN_PATH) \
+	  SKIP_INSTALL=NO \
+	  DSTROOT=/ \
+	  ONLY_ACTIVE_ARCH=NO
+
+install_adecl: dummy
+	xcodebuild install \
+	  -scheme adecl \
+	  -project $(PROJECT_NAME).xcodeproj \
+	  -destination="macOSX" \
+	  -configuration Release \
+	  -sdk macosx \
+	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+	  INSTALL_PATH=$(BIN_PATH) \
+	  SKIP_INSTALL=NO \
+	  DSTROOT=/ \
+	  ONLY_ACTIVE_ARCH=NO
+
+install_ares: dummy
+	xcodebuild install \
+	  -scheme ares \
+	  -project $(PROJECT_NAME).xcodeproj \
+	  -destination="macOSX" \
+	  -configuration Release \
+	  -sdk macosx \
+	  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+	  INSTALL_PATH=$(BIN_PATH) \
+	  SKIP_INSTALL=NO \
+	  DSTROOT=/ \
+	  ONLY_ACTIVE_ARCH=NO
+
+install_decls: dummy
+	(cd ../Resource && make)
+
+dummy:
+
